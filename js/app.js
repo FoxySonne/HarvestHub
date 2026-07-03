7b43473d6144d0514271841c79338afe01a990a7
FoxySonne <meeeeeeezzz@gmail.com>
2026-07-03 21:55:32 +0300

    Перенести запуск сайта

--- js/app.js
+++ js/app.js
@@ -1,21 +1,8 @@
-// После полной загрузки страницы
 window.addEventListener("DOMContentLoaded", () => {
+    loadBlock("sidebar-container", "components/sidebar.html");
+    loadBlock("rightbar-container", "components/rightbar.html");
+    loadBlock("main-title-container", "components/main-title.html");
 
-    loadBlock(
-        "sidebar-container",
-        "components/sidebar.html"
-    );
-
-    loadBlock(
-        "rightbar-container",
-        "components/rightbar.html"
-    );
-
-    loadBlock(
-        "main-title-container",
-        "components/main-title.html"
-        );
-
-    loadPage("home.html");
-
+    const lastPage = localStorage.getItem("currentPage") || "home.html";
+    loadPage(lastPage);
 });
