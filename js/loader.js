e31b3b2955fdf5848f108c776053b1b83fad6aac
FoxySonne <meeeeeeezzz@gmail.com>
2026-07-07 08:10:16 +0300

    Обновить кэш модулей после обучения войск

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,4 @@
-const SITE_ASSET_VERSION = "20260706-1";
+const SITE_ASSET_VERSION = "20260706-2";
 const QUICK_LINKS_STORAGE_KEY = "harvesthub_page_visits";
 const PAGE_FORM_STATE_PREFIX = "harvesthub_page_form_state:local:";
 const MAX_QUICK_LINKS = 5;
