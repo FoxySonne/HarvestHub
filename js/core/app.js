395e9b8b199afe1c27985840ef3debf7d0348511
FoxySonne <meeeeeeezzz@gmail.com>
2026-07-20 19:17:09 +0300

    Добавить запуск сайта в новую структуру

---
+++ js/core/app.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+  loadBlock("sidebar-container", "components/sidebar.html");
+  loadBlock("rightbar-container", "components/rightbar.html");
+  loadBlock("main-title-container", "components/main-title.html");
+
+  const lastPage = localStorage.getItem("currentPage") || "home.html";
+  loadPage(lastPage);
+});
